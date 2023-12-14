a9ff44fb90e9d922762fd44dfc75545440719472
olehandro <odpash.itmo@gmail.com>
2023-12-15 01:16:57 +0300

    raw

--- qualification_mode_service/server.py
+++ qualification_mode_service/server.py
@@ -1,24 +1,24 @@
 import asyncio
 import grpc
 from concurrent.futures import ThreadPoolExecutor
-from proto.qualification_pb2 import *
-from proto.qualification_pb2_grpc import QualificationServiceServicer, add_QualificationServiceServicer_to_server
+from proto import qualification_pb2, qualification_pb2_grpc
 
 
-class QualificationServiceImplementation(QualificationServiceServicer):
+
+class QualificationServiceImplementation(qualification_pb2_grpc.QualificationServiceServicer):
     async def ExecuteQualification(self, request, context):
         # Ваша асинхронная логика обработки запроса
         success = True
         message = "Qualification executed successfully"
         execution_time = 0.5  # Пример времени выполнения в секундах
 
-        response = QualificationResponse(success=success, data=message, execution_time=execution_time)
+        response = qualification_pb2.QualificationResponse(success=success, data=message, execution_time=execution_time)
         return response
 
 
 async def serve():
     server = grpc.aio.server(ThreadPoolExecutor(max_workers=10))
-    add_QualificationServiceServicer_to_server(QualificationServiceImplementation(), server)
+    qualification_pb2_grpc.add_QualificationServiceServicer_to_server(QualificationServiceImplementation(), server)
     server.add_insecure_port('[::]:50054')
     await server.start()
     await server.wait_for_termination()
