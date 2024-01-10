d58aae0d387c6befb5db056e75a3380e390d31d5
olehandro <odpash.itmo@gmail.com>
2024-01-10 11:35:43 +0300

    'Added support manager intervented fix'

--- prompt_mode_service/server.py
+++ prompt_mode_service/server.py
@@ -10,11 +10,18 @@ from proto import prompt_mode_pb2, prompt_mode_pb2_grpc
 
 
 async def complete_openai(prompt, model, max_tokens, temperature, api_token):
-    async with openai.AsyncOpenAI(api_key=api_token) as client:
-        completion = await client.chat.completions.create(
-            model=model, messages=prompt, max_tokens=max_tokens, temperature=temperature
-        )
-        return completion.choices[0].message.content
+    try:
+        async with openai.AsyncOpenAI(api_key=api_token) as client:
+            completion = await client.chat.completions.create(
+                model=model, messages=prompt, max_tokens=max_tokens, temperature=temperature
+            )
+            return completion.choices[0].message.content
+    except:
+        async with openai.AsyncOpenAI(api_key=api_token) as client:
+            completion = await client.chat.completions.create(
+                model='gpt-3.5-turbo', messages=prompt, max_tokens=max_tokens, temperature=temperature
+            )
+            return completion.choices[0].message.content
 
 
 class OpenAIPromptServicer(prompt_mode_pb2_grpc.OpenAIPromptServiceServicer):
