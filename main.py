f28066747e523b8a3ed740c3b487d67d71242c87
olehandro <odpash.itmo@gmail.com>
2024-01-10 11:24:16 +0300

    'Added support manager intervented fix'

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import asyncio
 
 
 async def send_message_to_amocrm(setting, message, text, is_bot):
+    print('Отправляю сообщение', text, message.chat.id)
     message_id = await amocrm.send_message(
         setting.amo_host,
         setting.amo_email,
--- prompt_mode_service/client.py
+++ prompt_mode_service/client.py
@@ -14,6 +14,7 @@ server_host = os.getenv("SERVER_HOST_EN") + ":50052"
 
 
 async def run(messages, model, max_tokens, temperature, api_token):
+    print('Запустил prompt мод')
     channel = grpc.aio.insecure_channel(server_host)
     stub = OpenAIPromptServiceStub(channel)
 
