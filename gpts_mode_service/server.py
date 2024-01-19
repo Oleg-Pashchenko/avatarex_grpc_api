d509c7ecb322de89d4d57e954372c831acc7020f
olehandro <odpash.itmo@gmail.com>
2024-01-19 21:52:01 +0300

    'Added support manager intervented fix'

--- gpts_mode_service/server.py
+++ gpts_mode_service/server.py
@@ -4,21 +4,26 @@ import impl
 
 
 async def handle(request):
-    data = await request.json()
+    try:
+        data = await request.json()
 
-    # Access the required parameters
-    question = data.get("question", '')
-    token = data.get("token", '')
-    thread_id = data.get("thread_id", '')
-    assistant_id = data.get("assistant_id", '')
-    text, thread_id = await impl.execute(question=question,
-                                         token=token,
-                                         thread_id=thread_id,
-                                         assistant_id=assistant_id)
+        # Access the required parameters
+        question = data.get("question", '')
+        token = data.get("token", '')
+        thread_id = data.get("thread_id", '')
+        assistant_id = data.get("assistant_id", '')
+        text, thread_id = await impl.execute(question=question,
+                                             token=token,
+                                             thread_id=thread_id,
+                                             assistant_id=assistant_id)
 
-    return web.Response(
-        text=f"{thread_id}|||{text}", content_type="application/json"
-    )
+        return web.Response(
+            text=f"{thread_id}|||{text}", content_type="application/json"
+        )
+    except:
+        return web.Response(
+            text='Ошибка настроек', content_type='application/json'
+        )
 
 
 app = web.Application()
