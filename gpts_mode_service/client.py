7482b67cbb805d8970b29ee3e2f7c7615edec2d3
olehandro <odpash.itmo@gmail.com>
2024-01-19 19:38:17 +0300

    'Added support manager intervented fix'

--- gpts_mode_service/client.py
+++ gpts_mode_service/client.py
@@ -15,11 +15,3 @@ async def send_request(request):
         async with session.post(server_url, json=request) as response:
             return await response.text()
 
-
-if __name__ == '__main__':
-    print(asyncio.run(send_request({
-        'question': 'Привет',
-        'token': '',
-        'thread_id': None,
-        'assistant_id': 'asst_KwmbWZ73fMCLA9my8Wy86MNm'}
-    )))
