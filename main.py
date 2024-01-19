f171d5271c4019ddb052b663c5387bcd0a557ff5
olehandro <odpash.itmo@gmail.com>
2024-01-19 19:42:39 +0300

    'Added support manager intervented fix'

--- main.py
+++ main.py
@@ -164,6 +164,7 @@ async def process_settings(setting):
         setting.pipeline_id,
         setting.statuses_ids,
     )
+    print(messages)
     # Список задач для параллельной обработки сообщений
     tasks = []
     for message in messages.answer:
