3aa8133133d0a3c931367e76ce552ea4a7161a77
olehandro <odpash.itmo@gmail.com>
2024-01-24 18:40:22 +0300

    'Added support manager intervented fix'

--- qualification_mode_service/implementation.py
+++ qualification_mode_service/implementation.py
@@ -4,6 +4,16 @@ import openai
 
 
 async def qualification_passed(question, field, message, openai_key):
+    """
+    Какой у вас тариф?
+    {'id': 1278951, 'name': 'Тариф', 'type': 'select', 'active_value': None,
+     'possible_values': [{'id': 744363, 'value': 'Профи', 'sort': 500},
+      {'id': 744365, 'value': 'Бизнес', 'sort': 1},
+       {'id': 744367, 'value': 'Приват', 'sort': 2}]}
+     профи
+
+    """
+
     if field['type'] != 'field':
         required = ['param']
         properties = {'param': {'type': 'string', 'enum': []}}
@@ -18,7 +28,7 @@ async def qualification_passed(question, field, message, openai_key):
                            }}
     func = [{
         "name": "Function",
-        "description": "Function description",
+        "description": "Является ли ответ пользователя на заданный вопрос",
         "parameters": {
             "type": "object",
             "properties": properties,
@@ -28,21 +38,24 @@ async def qualification_passed(question, field, message, openai_key):
 
     try:
         messages = [
-            {'role': 'system', 'content': 'Give answer:'},
+            {'role': 'system', 'content': 'Является ли ответ пользователя на заданный вопрос'},
             {"role": "user",
              "content": message}]
 
         async with openai.AsyncOpenAI(api_key=openai_key) as client:
 
-            response = await client.chat.completions.create(model='gpt-4',
+            response = await client.chat.completions.create(model='gpt-3.5-turbo',
                                                             messages=messages,
                                                             functions=func,
                                                             function_call="auto")
         response_message = response.choices[0].message
-    except:
+        print(response_message)
+    except Exception as e:
+        print(e)
         return False
     if response_message.function_call:
         function_args = json.loads(response_message.function_call.arguments)
+        print(function_args)
         if 'is_correct' in function_args:
             if function_args['is_correct'] is True:
                 return True, message
