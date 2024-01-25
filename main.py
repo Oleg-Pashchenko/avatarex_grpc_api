7b32d2f059a8e500608359a358d6a497cbd6d178
olehandro <odpash.itmo@gmail.com>
2024-01-25 11:39:59 +0300

    'Added support manager intervented fix'

--- main.py
+++ main.py
@@ -204,7 +204,17 @@ async def process_message(message, setting):
                 }
             )
     if not qualification_answer['qualification_status']:
-        without_questions_answer = delete_questions(answer_to_sent)
+        # without_questions_answer = delete_questions(answer_to_sent)
+        without_questions_answer = await prompt_mode.run(
+            messages=[{'role': 'system', 'content': 'Переформулируй без вопросов'},
+                      {'role': 'user', 'content': answer_to_sent}],
+            model=setting.model_title,
+            api_token=setting.api_token,
+            max_tokens=setting.max_tokens,
+            temperature=setting.temperature,
+        )
+        without_questions_answer = without_questions_answer.data.message
+
         await send_message_to_amocrm(setting, message, without_questions_answer + '\n' + qualification_answer['message'], True)
     else:
         await send_message_to_amocrm(setting, message, answer_to_sent, True)
