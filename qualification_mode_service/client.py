2332f1fb31e9f30b8f38862f8be0a9b07cf3e676
olehandro <odpash.itmo@gmail.com>
2024-02-01 14:41:52 +0300

    'Added support manager intervented fix'

--- qualification_mode_service/client.py
+++ qualification_mode_service/client.py
@@ -7,7 +7,7 @@ import os
 
 dotenv.load_dotenv()
 
-server_url = 'http://' + os.getenv('SERVER_HOST_EN') + ":50054"
+server_url = 'http://' + os.getenv('SERVER_HOST_EN') + ":8888"
 
 
 async def send_request(request):
@@ -15,4 +15,10 @@ async def send_request(request):
         async with session.post(server_url, json=request) as response:
             response_text = await response.text()
             response_json = json.loads(response_text)
-            return response_json
+            resp = response_json['answer']
+            resp['pipeline_id'] = request['pipeline']
+            resp['amo_host'] = request['host']
+            resp['amo_email'] = request['password']
+            resp['amo_password'] = request['email']
+            resp['lead_id'] = request['lead_id']
+            return resp
