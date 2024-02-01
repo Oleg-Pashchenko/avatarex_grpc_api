6ba3834323a19e7ed5ea43d0ba6162cca6b2be81
olehandro <odpash.itmo@gmail.com>
2024-02-01 14:58:25 +0300

    'Added support manager intervented fix'

--- qualification_mode_service/client.py
+++ qualification_mode_service/client.py
@@ -15,9 +15,9 @@ async def send_request(request):
         async with session.post(server_url, json=request) as response:
             response_json = await response.json()
             resp = response_json['answer']
-            resp['pipeline_id'] = request['pipeline']
-            resp['amo_host'] = request['host']
-            resp['amo_email'] = request['password']
-            resp['amo_password'] = request['email']
-            resp['lead_id'] = request['lead_id']
+            resp['fill_command']['pipeline_id'] = request['pipeline']
+            resp['fill_command']['amo_host'] = request['host']
+            resp['fill_command']['amo_email'] = request['password']
+            resp['fill_command']['amo_password'] = request['email']
+            resp['fill_command']['lead_id'] = request['lead_id']
             return resp
