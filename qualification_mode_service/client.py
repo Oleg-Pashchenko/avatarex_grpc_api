12c7202377a38de1ea0e7b2630966da20ebc6246
olehandro <odpash.itmo@gmail.com>
2024-02-01 14:43:30 +0300

    'Added support manager intervented fix'

--- qualification_mode_service/client.py
+++ qualification_mode_service/client.py
@@ -13,8 +13,7 @@ server_url = 'http://' + os.getenv('SERVER_HOST_EN') + ":8888"
 async def send_request(request):
     async with aiohttp.ClientSession() as session:
         async with session.post(server_url, json=request) as response:
-            response_text = await response.text()
-            response_json = json.loads(response_text)
+            response_json = await response.json()
             resp = response_json['answer']
             resp['pipeline_id'] = request['pipeline']
             resp['amo_host'] = request['host']
