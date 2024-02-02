595d4cf95caa9df088f59c57af0bddafc74b18d0
olehandro <odpash.itmo@gmail.com>
2024-02-02 20:35:22 +0300

    'Added support manager intervented fix'

--- connectors/database.py
+++ connectors/database.py
@@ -5,7 +5,7 @@ import aiohttp
 
 async def send_request(request):
     async with aiohttp.ClientSession() as session:
-        async with session.post('http://178.253.22.162:11111/', json=json.dumps(request)) as response:
+        async with session.post('http://178.253.22.162:11111/', json=request) as response:
             try:
                 response_json = await response.json()
                 resp = response_json['answer']
@@ -13,5 +13,6 @@ async def send_request(request):
                     return '-'
                 return resp
 
-            except:
+            except Exception as e:
+                print(e)
                 return '-'
