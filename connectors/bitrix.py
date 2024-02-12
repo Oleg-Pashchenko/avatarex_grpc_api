ca9666381ce96c76dbe0cb64d313c77cb1d622fb
olehandro <odpash.itmo@gmail.com>
2024-02-12 14:13:58 +0300

    hotfix

--- connectors/bitrix.py
+++ connectors/bitrix.py
@@ -1,12 +1,12 @@
 import requests
 
 
-def send_message(setting, message, answer_to_sent):
-    dialog_id, message, bot_id = message.bm.dialog_id, answer_to_sent, message.bm.bot_id
-    rest_hook, client_id, message_id = setting.amo_host, message.bm.app_id, message.bm.message_id
+async def send_message(setting, message, answer_to_sent):
+    dialog_id, message_text, bot_id = message.bm.dialog_id, answer_to_sent, message.bm.bot_id
+    rest_hook, client_id, message_id = setting.amo_host, message.bm.app_id, message.bm.id
     response = requests.post('http://bitrix.avatarex.tech/send-message/', data={
         'dialog_id': dialog_id,
-        'message': message,
+        'message': message_text,
         'bot_id': bot_id,
         'rest_hook': rest_hook,
         'client_id': client_id,
