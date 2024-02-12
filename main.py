a3a2690be4198c58806ad744dd4bd13492d5c59b
olehandro <odpash.itmo@gmail.com>
2024-02-12 13:58:12 +0300

    hotfix

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ async def process_bitrix(message, setting):
 async def process_settings(setting):
     st = time.time()
     tasks = []
-
+    print(setting.amo_host)
     if '-' == setting.amo_email:
         print('yes')
         setting.statuses_ids = ['NEW', 'PREPARATION']
@@ -156,8 +156,6 @@ async def cycle():
         settings: list[ApiSettings] = get_enabled_api_settings()
         print(len(settings))
         for setting in settings:
-            if 'bitrix' in setting.amo_host:
-                print(setting.amo_host)
             if os.getenv('MODE') == 'testing':
                 if 'chatgpt.amocrm' in setting.amo_host:
                     asyncio.ensure_future(process_settings(setting))
