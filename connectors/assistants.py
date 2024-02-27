e0bc9db836725e7bc8ece1b84540beacc0e79ef0
olehandro <odpash.itmo@gmail.com>
2024-02-27 19:45:16 +0300

    added bitrix support

--- connectors/assistants.py
+++ connectors/assistants.py
@@ -4,11 +4,12 @@ from database_connect_service.src.site import ApiSettings
 
 
 async def get_answer(message, setting: ApiSettings):
+    print(message)
     return await connector.send_request(
         request={
             "assistant_id": setting.assistant_id,
             'api_token': setting.api_token,
-            'question': message['message'],
+            'question': message['answer'],
             'thread_id': get_thread_by_lead_id(message['lead_id'])
         },
         url='http://178.253.22.162:9999/'
